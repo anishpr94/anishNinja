c94f40774d164d1caa1eadf29e39d84c3774f686
“anpr” <anish.pr@oracle.com>
2018-05-04 00:16:51 +0530

    heroku

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post('/form', function(req, res){
  
   var nodemailer = require('nodemailer');
   console.log('gmai111l ready');
-  var transporter = nodemailer.createTransport("SMTP",{
+  var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: 'anishpr94@gmail.com',
