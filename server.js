f0dfd13c454332a73918f35479ae790011600e88
“anpr” <anish.pr@oracle.com>
2018-05-04 00:05:50 +0530

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app     = express();
 var path    = require("path");
+var bodyParser = require('body-parser');
 
 app.use(express.static('public'))
 app.get('/',function(req,res){
@@ -19,4 +20,58 @@ app.listen(process.env.PORT || 3000, function(){
   console.log('listening on',this.address().port);
 });
 
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 console.log("Running at Port 3000");
+
+
+app.post('/form', function(req, res){
+  res.setHeader('Content-Type', 'application/json');
+
+  //mimic a slow network connection
+  setTimeout(function(){
+
+      // res.send(JSON.stringify({
+      //     name: req.body.name || null,
+      //     email: req.body.email || null,
+      //     message: req.body.message || null
+      // }));
+      res.send(
+        'Message Sent Successfully! Thank you!'
+      );
+
+ 
+  var nodemailer = require('nodemailer');
+  console.log('gmai111l ready');
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'anishpr94@gmail.com',
+      pass: 'herokupass'
+    }
+  });
+  console.log('gmail ready');
+  
+  var mailOptions = {
+    from: req.body.email,
+    to: req.body.email,
+    subject: 'Response from gypsy camp',
+    text: req.body.message
+  };
+  console.log(mailOptions);
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+}, 1000)
+  //debugging output for the terminal
+  console.log('you posted: First Name: ' + req.body.name + ', Last Name: ' + req.body.email);
+});
